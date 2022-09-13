56c7d7b83f81a93e3dc940bd6c3dd6f87d66f460
t-nakai10 <bad.sideline.999@gmail.com>
2022-09-13 22:46:40 +0900

    親コンポーネントで定義された todos を props として受け取る

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-const TodoList = () => {
+/**
+ * 親コンポーネントの useState の todos を受け取るには props を引数に渡す.
+ * 引数に props を受け取り props.todos とできるが、一般的にはオブジェクトで記載
+ **/
+const TodoList = ({ todos }) => {
   return (
     <div>
-      <p>todolist</p>
+      <p>{todos}</p>
     </div>
   );
 };
