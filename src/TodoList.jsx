0cc54361f3add600963b5b523217074c4ff67793
t-nakai10 <bad.sideline.999@gmail.com>
2022-09-14 06:47:58 +0900

    Todo のリスト内のコンポーネントは別途作成

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,15 +1,13 @@
 import React from "react";
+import Todo from "Todo";
 
 /**
  * 親コンポーネントの useState の todos を受け取るには props を引数に渡す.
  * 引数に props を受け取り props.todos とできるが、一般的にはオブジェクトで記載
  **/
 const TodoList = ({ todos }) => {
-  return (
-    <div>
-      <p>{todos}</p>
-    </div>
-  );
+  // 本来は好ましくないリレー.
+  return todos.map((todo) => <Todo todo={todo} key={todo} />);
 };
 
 export default TodoList;
