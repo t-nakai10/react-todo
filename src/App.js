89667cbbeacfe0184294447c9e05842e6e44ed47
t-nakai10 <bad.sideline.999@gmail.com>
2022-09-13 22:40:36 +0900

    hooks: useState を使用. 子コンポーネントに渡す

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import TodoList from "TodoList";
 
 function App() {
+  /**
+   * useState() hooks で状態を管理.
+   * - 引数に初期値を設定できる.
+   **/
+  const [todos, setTodos] = useState(["Todo1", "Todo2"]);
+
   return (
     <div className="App">
-      <TodoList />
+      <TodoList todos={todos} />
       <input type="text" />
       <button>タスクを追加</button>
       <button>完了したタスクを削除</button>
