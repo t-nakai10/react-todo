0009cc13301b6fdb0efc410655a88fc54425b6ec
t-nakai10 <bad.sideline.999@gmail.com>
2022-09-13 22:22:55 +0900

    Todo List コンポーネントを作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import "./App.css";
-
+import TodoList from "TodoList";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
 }
 
 export default App;
