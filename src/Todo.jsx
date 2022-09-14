09063579342bfb3da9c6513c9f7d64b62e82743b
t-nakai10 <bad.sideline.999@gmail.com>
2022-09-14 23:10:58 +0900

    子コンポーネントにprops として渡し onChange を使用して値を更新する

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo, toggleTodo }) => {
+  // input が変更されたとき toggleTodo を呼び出し、値を更新する.
+  const handleTodoClick = () => {
+    toggleTodo(todo.id);
+  };
+
   return (
     <div>
       <label>
-        <input type="checkbox" checked={todo.completed} readOnly />
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={handleTodoClick}
+          readOnly
+        />
       </label>
       {todo.name}
     </div>
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -5,9 +5,12 @@ import Todo from "Todo";
  * 親コンポーネントの useState の todos を受け取るには props を引数に渡す.
  * 引数に props を受け取り props.todos とできるが、一般的にはオブジェクトで記載
  **/
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, toggleTodo }) => {
   // 本来は好ましくないリレー.
-  return todos.map((todo) => <Todo todo={todo} key={todo.id} />);
+  return todos.map((todo) => (
+    // Todo に渡していく.
+    <Todo todo={todo} key={todo.id} toggleTodo={toggleTodo} />
+  ));
 };
 
 export default TodoList;
